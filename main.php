345e411c8ecc09116d02def35dabe85a168ab262
debrouta <taydebroux@gmail.com>
2014-10-02 17:16:08 -0500

    calendar
    main.php is an example of a possible calendar

---
+++ main.php
@@ -0,0 +1,5 @@
+<?php
+    echo 'Hello world!';
+?>
+
+<div id="upto-calendar-container-0TOO" class="upto-calendar-plugin"><script>!function(c,b,a){function d(h){var e=document.getElementById("upto-calendar-container-"+c),f=document.createElement("SCRIPT"),g=" http://upto.com/js/plugin.js";f.onreadystatechange=function(){if(f.readyState==="loaded"||f.readyState==="complete"){f.onreadystatechange=null;h()}};f.onload=function(){h()};f.setAttribute("src",g);e.parentNode.insertBefore(f,e)}d(function(){upto.init(c)})}("0TOO");</script></div>
