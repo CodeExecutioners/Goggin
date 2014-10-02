9b1e2f1c967bb855df397c7db51100a5ffee12c1
fisherrm <fisherr@uwec.edu>
2014-10-02 16:53:49 -0500

    arcana template

--- main.py
+++ main.py
@@ -14,11 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import urllib
 import webapp2
+import jinja2
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+	
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #self.response.write('Hello world!')
+		template_values ={}
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
+		
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
